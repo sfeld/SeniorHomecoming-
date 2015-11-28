015cc3944034e399370bfb6948738a9e1da3e5d5
sfeld <samuel.feldman@gmail.com>
2015-11-27 20:41:59 -0800

    Trying to get it to fit to screen

--- spirit.js
+++ spirit.js
@@ -76,8 +76,8 @@ var tickLength = 10;
 //}
 //I added the code to my drawing function
 function update() {
-  	//ctx1.canvas.width  = window.innerWidth;
-  	//ctx1.canvas.height = window.innerHeight;
+  	ctx1.canvas.width  = window.innerWidth;
+  	ctx1.canvas.height = window.innerHeight;
 	ctx1.fillStyle = "white";
 	ctx1.fillRect(0,0,canvas.width,canvas.height)
 	
